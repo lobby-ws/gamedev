1627db03ad21b4f682c3362d77d1da591e0449d1
saori-eth <D84da@proton.me>
2026-01-29 23:09:41 -0600

    scale culling with splat in splat node

--- src/core/nodes/Splat.js
+++ src/core/nodes/Splat.js
@@ -5,6 +5,7 @@ export class Splat extends Node {
     super(data)
     this.name = 'splat'
     this._mesh = data.mesh || null
+    this._originalRadius = null
   }
 
   mount() {
@@ -13,6 +14,11 @@ export class Splat extends Node {
     this._mesh.matrix.copy(this.matrixWorld)
     this._mesh.matrixAutoUpdate = false
     this._mesh.updateMatrixWorld(true)
+    // Store original bounding sphere radius for scaling
+    if (this._mesh.geometry?.boundingSphere) {
+      this._originalRadius = this._mesh.geometry.boundingSphere.radius
+    }
+    this.updateBounds()
     this.ctx.world.stage.scene.add(this._mesh)
   }
 
@@ -20,7 +26,14 @@ export class Splat extends Node {
     if (didMove && this._mesh) {
       this._mesh.matrix.copy(this.matrixWorld)
       this._mesh.updateMatrixWorld(true)
+      this.updateBounds()
     }
+  }
+
+  updateBounds() {
+    if (!this._originalRadius || !this._mesh.geometry?.boundingSphere) return
+    const maxScale = Math.max(this.scale.x, this.scale.y, this.scale.z)
+    this._mesh.geometry.boundingSphere.radius = this._originalRadius * maxScale
   }
 
   unmount() {
@@ -32,6 +45,7 @@ export class Splat extends Node {
   copy(source, recursive) {
     super.copy(source, recursive)
     this._mesh = source._mesh
+    this._originalRadius = source._originalRadius
     return this
   }
 
